c8b8dc163270b8a91dcb8a10d76a7437d8860155
Azhem <e.constanciovergara@gmail.com>
2024-10-21 22:09:58 -0300

    inicios de JS

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,29 @@
+// Variables
+//JavaScript de tipo Dinámico (Java, CSharp, C)
+var producto = 'Audífonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; //Iniciamos la variable sin valor
+
+// producto = true; //reasignando valor de la variable
+
+disponible = true;
+
+// puedes asignar varias variables seguidas:
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Variables no permitidas
+// var 1disponible;
+// var -disponible;
+
+//Variables permitidas:
+var disponible_;
+// Estilos para las variables:
+var nombre_producto = 'Monitor HD'; //Underscore
+var nombreProducto = 'Monitor HD'; //Camelcase
+var NombreProducto = 'Monitor HD'; //Pascal Case
+var nombreproducto = 'Monitor HD'; //noname
+
+//**Las variables son CASE SENSITIVE */
+console.log(producto);
