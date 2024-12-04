8af83920689b4cfeadebcda4f173729eba4e4b02
Edu Constancio <e.constanciovergara@gmail.com>
2024-12-04 17:13:28 -0300

    add 06.js

--- IntroduccionJS/js/05.js
+++ IntroduccionJS/js/05.js
@@ -1,5 +1,16 @@
 const numero1 = 100;
 const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = .1020;
+const numero5 = -5;
 
-console.log(numero1);
-console.log(numero2);
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero2 % numero1);
+
+const cliente = 'Juan';
+const cliente2 = 'Pablo';
+// esto daría el error en consola: NaN (Not a Number)
+console.log(cliente + cliente2);
