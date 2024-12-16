6d6c55e1bc3a9357a452d8e2652f7417e8d37cfc
Azhem <e.constanciovergara@gmail.com>
2024-12-16 17:36:27 -0300

    funciones Inputs y TextAreas

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -31,28 +31,85 @@ const navegacion = document.querySelector('.navegacion');
 navegacion.appendChild(nuevoEnlace);
 
 // Eventos
-console.log(1);
+// console.log(1);
 
-window.addEventListener('load', function() { //load espera a que el JS y los archivos que dependen del HTML estén listos
-    console.log(2);
-})
-window.onload = function() {
-    console.log(3);
+// window.addEventListener('load', function() { //load espera a que el JS y los archivos que dependen del HTML estén listos
+//     console.log(2);
+// })
+// window.onload = function() {
+//     console.log(3);
+// }
+// document.addEventListener('DOMContentLoaded', function() { // sólo espera por el HTML, pero no espera CSS o imágenes.
+//     console.log(4)
+// })
+
+// console.log(5);
+
+// function imprimir() { 
+//     console.log(2);
+// }
+// // una función normal puede ser llamada dentro 
+// // del addEventListener para poder usarse, de esta manera quedaría:
+// addEventListener('load', imprimir);
+
+// //Ejemplo de función que se activa al hacer scroll
+// onscroll = function() {
+//     console.log('Scrolling...')
+// }
+
+// Seleccionar elementos y asociarles un evento
+
+// const btnEnviar = document.querySelector('.boton--primario');
+
+// btnEnviar.addEventListener('click', function(evento){
+//     console.log(evento);
+//     //Validar un formulario para verificar que los campos estén llenos
+//     evento.preventDefault();  //muy usado en formularios
+//     console.log('Enviando Formulario');
+// });
+
+// Eventos de los Inputs y TextAreas
+// Validaciones:
+// input : se activa apenas cambia el valor dentro del elemento.
+// change : se activa cuando el texto ha cambiado al perder el foco.
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
 }
-document.addEventListener('DOMContentLoaded', function() { // sólo espera por el HTML, pero no espera CSS o imágenes.
-    console.log(4)
-})
 
-console.log(5);
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
 
-function imprimir() { 
-    console.log(2);
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e) {
+    datos[e.target.id] = e.target.value;
+    console.log(datos);
 }
-// una función normal puede ser llamada dentro 
-// del addEventListener para poder usarse, de esta manera quedaría:
-addEventListener('load', imprimir);
 
-//Ejemplo de función que se activa al hacer scroll
-onscroll = function() {
-    console.log('Scrolling...')
-}
+// Validando el envío: *Extra - no estaba en el curso - Desafío*
+const btnEnviar = document.querySelector('.boton--primario');
+
+btnEnviar.addEventListener('click', function(){
+    const valNombre = datos.nombre;
+    const valEmail = datos.email;
+    const valMensaje = datos.mensaje;
+    if (valNombre == '') {
+        evento.preventDefault(); 
+        console.log('Debe agregar el nombre');
+    } else if (valEmail == ''){
+        evento.preventDefault();
+        console.log('Debe agregar el Email');
+    } else if (valMensaje == ''){
+        evento.preventDefault();
+        console.log('Debe agregar un Mensaje');
+    } else {
+        //Validar un formulario para verificar que los campos estén llenos
+        evento.preventDefault();  //muy usado en formularios
+        console.log('Enviando Formulario');
+    }
+});
